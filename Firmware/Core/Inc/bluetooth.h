028865dc82a9965a3c0d03335a0f63480fd77f5c
Elham Islam <eislam@umich.edu>
2023-10-19 13:00:45 -0400

    bluetooth module done lel

--- Firmware/Core/Inc/bluetooth.h
+++ Firmware/Core/Inc/bluetooth.h
@@ -11,31 +11,22 @@
 #include "stm32l4xx_hal.h"
 #include <stdint.h>
 
-#define SLEEP_MODE "AT+SLEEP"
-#define AT "AT"
-#define QUERY_ADDR "AT+ADDR?"
-#define QUERY_BAUD "AT+BAUD?"
-#define CONNECT_TO_LAST "AT+CONNL"
-#define CONNECT_TO_ADDRS "AT+CO"
-
-#define ACCEPTED_REQUEST "OK+CONNA"
-#define CONNECTED "OK+CONN"
-#define CONNECTION_ERROR "OK+CONNE"
-#define CONNECTION_FAILED "OK+CONNF"
-#define DISCOVERY_SCAN "AT+DISC?"
-#define QUERY_MODE "AT+MODE?"
-
-#define QUERY_NAME "AT+NAME?"
-
-#define QUERY_POWER "AT+PCTL"
-
-#define SAVE_ADDRS "AT+SAVE?"
-
-#define SHOW_NAME  "AT+SHOW"
-
-
-
-
+//#define SLEEP_MODE "AT+SLEEP"
+//#define AT "AT"
+//#define QUERY_ADDR "AT+ADDR?"
+//#define QUERY_BAUD "AT+BAUD?"
+//#define CONNECT_TO_LAST "AT+CONNL"
+//#define CONNECT_TO_ADDRS "AT+CO"
+//#define ACCEPTED_REQUEST "OK+CONNA"
+//#define CONNECTED "OK+CONN"
+//#define CONNECTION_ERROR "OK+CONNE"
+//#define CONNECTION_FAILED "OK+CONNF"
+//#define DISCOVERY_SCAN "AT+DISC?"
+//#define QUERY_MODE "AT+MODE?"
+//#define QUERY_NAME "AT+NAME?"
+//#define QUERY_POWER "AT+PCTL"
+//#define SAVE_ADDRS "AT+SAVE?"
+//#define SHOW_NAME  "AT+SHOW"
 
 
 typedef struct BLE_interface
@@ -50,19 +41,6 @@ typedef struct BLE_interface
 	uint8_t addr[6]; // bluetooth addr is 6 bytes, colon separated
 } BLE_interface;
 
-// initializes BLE interface
-void initBLE(BLE_interface * ble);
-
-void setDeviceName(char* name, BLE_interface* ble);
-
-// check if device is connected to target
-bool isConnected(char* target, BLE_interface* ble);
-
-// perform any maintenance to connect and disconnect to target
-// return status
-uint32_t connect(char* target, BLE_interface* ble);
-
-uint32_t disconnect(char* target, BLE_interface* ble);
 
 // poll for events
 Void readBLE(uint32_t timeout, BLE_interface* ble, uint8_t* rx_buff);
--- Firmware/Core/Src/bluetooth.c
+++ Firmware/Core/Src/bluetooth.c
@@ -5,4 +5,14 @@
  *      Author: evanm
  */
 #include "bluetooth.h"
+#define MAX_JSON_BYTE_SIZE 128
 
+
+
+Void readBLE(uint32_t timeout, BLE_interface* ble, uint8_t* rx_buff){
+	HAL_UART_Receive(ble->huart, rx_buff, sizeof(rx_buff), MAX_JSON_BYTE_SIZE);// Sending in normal mode
+}
+
+uint32_t broadcast(uint32_t* tx_buff, uint32_t size, BLE_interface* ble){
+	HAL_UART_Transmit(ble->huart, tx_buff, size,10);// Sending in normal mode
+}
