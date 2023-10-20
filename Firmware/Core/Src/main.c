51a383c6c2f0e3fe899b20d2a9552d1377b58534
Elham Islam <eislam@umich.edu>
2023-10-20 16:20:12 -0400

    update ioc with rf select lines and added iteration through antennas in the read antenna array function

--- Firmware/Core/Src/main.c
+++ Firmware/Core/Src/main.c
@@ -502,7 +502,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(SMPS_V1_GPIO_Port, SMPS_V1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|LD4_Pin
+                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+                          |GPIO_PIN_7, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -517,12 +519,16 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(SMPS_V1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD4_Pin */
-  GPIO_InitStruct.Pin = LD4_Pin;
+  /*Configure GPIO pins : PB0 PB1 PB2 LD4_Pin
+                           PB3 PB4 PB5 PB6
+                           PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|LD4_Pin
+                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+                          |GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD4_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
--- Firmware/Core/Src/reader.c
+++ Firmware/Core/Src/reader.c
@@ -5,6 +5,11 @@
  *      Author: evanm
  */
 #include "reader.h"
+#include "stm32l4xx_hal.h"
+
+#define SEL_PORT GPIOB
+
+const uint16_t sel[8] = {GPIO_PIN_0, GPIO_PIN_1, GPIO_PIN_2, GPIO_PIN_3, GPIO_PIN_4, GPIO_PIN_5, GPIO_PIN_6, GPIO_PIN_7};
 
 // global variable for RFID tag info to be tracked
 BeanBag_interface BagInfo[8];
@@ -30,4 +35,15 @@ void calculateRawScore(uint8_t* teamRawScore, bool isTeam1) {
 // reads all RFID antenna regions and updates the global BagStatus array
 void RFID_readArray(void) {
 
+	for(uint8_t row = 0; row < 3; row++){
+		// iterate through sel6 and sel7
+	    HAL_GPIO_WritePin(SEL_PORT, sel[6], (row >> 1) & 0b01);
+	    HAL_GPIO_WritePin(SEL_PORT, sel[7], (row & 0b01) );
+
+		for(uint8_t column = 0; column < 4; column++){
+			HAL_GPIO_WritePin(SEL_PORT, sel[row*2], (column >> 1) & 0b01);
+			HAL_GPIO_WritePin(SEL_PORT, sel[row*2 + 1], (column & 0b01) );
+
+		}
+	}
 }
